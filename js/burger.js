d8d6c2d60449e4fc058b7e35ec66393df0ba4f84
Ha0rej1 <38035478+Ha0rej1@users.noreply.github.com>
2019-07-11 00:18:53 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,31 @@
 var menu = document.getElementById('mobmenu');
-var open = false; 
-function callmenu(){
-	if (open==false) {
-		menu.classList.add('mobile-menu-left');
-		open=true;
-	}
-	else{
-		menu.classList.remove('mobile-menu-left');
-		open=false;
-	}
+var open = false;
+var burger = document.getElementById('burgermen');
+
+function callmenu() {
+    if (open == false) {
+        open = true;
+        menu.classList.add('mobile-menu-move');
+        burger.classList.add('menu-burger-move')
+    } else {
+        open = false;
+        menu.classList.remove('mobile-menu-move');
+        burger.classList.remove('menu-burger-move');
+
+    }
 }
 var projects = document.getElementById('projects');
-function showProjects(){
-	projects.classList.remove('dnone');
+
+function showProjects() {
+    projects.classList.remove('dnone');
+        open = false;
+    menu.classList.remove('mobile-menu-move');
+    burger.classList.remove('menu-burger-move');
 }
+
+/*function closemenu() {
+    open = false;
+    menu.classList.remove('mobile-menu-move');
+    burger.classList.remove('menu-burger-move');
+}
+*/
