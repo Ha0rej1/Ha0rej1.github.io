912a40b64f67783db0fdccaf8bf0b24186b1e11e
kgbsenior <Rostik3021@gmail.com>
2020-04-30 22:05:10 +0300

    frame release

--- aveone/aveonescript.js
+++ aveone/aveonescript.js
@@ -1,3 +1,14 @@
+var theme = "Light";
+function changeTheme(){
+  if(theme=="Light"){
+    document.body.classList.add("dark");
+    theme="Dark";
+  }
+  else{
+    document.body.classList.remove("dark");
+    theme="Light";
+  }
+}
 var gotop = $('#gotop');
 
 $(window).scroll(function () {
@@ -7,3 +18,18 @@ $(window).scroll(function () {
     gotop.removeClass('gotop-show');
   }
 });
+
+var toastcounter = -1;
+
+    function toast(a) {
+        toastcounter++;
+        var savecounter = toastcounter;
+        document.body.insertAdjacentHTML("beforeend", `<div class="toast customfadeInUp shadow"></div>`);
+
+        var x = document.getElementsByClassName("toast");
+        x[savecounter].insertAdjacentHTML("afterbegin", a);
+        setTimeout(function () {
+            x[savecounter].classList.remove("customfadeInUp");
+            x[savecounter].classList.add("customfadeOutDown");
+        }, 1500);
+    }
