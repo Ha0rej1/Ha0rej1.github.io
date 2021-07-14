3716d439b4c97b7971e36a6c3a76ea00be8aaa81
kgbsenior <Rostik3014@gmail.com>
2021-07-14 03:37:03 +0300

    new animations, new sections

--- musician/js/script.js
+++ musician/js/script.js
@@ -1,8 +1,9 @@
 let artname = document.getElementById("artname");
-let header = document.getElementById("header")
+let header = document.getElementById("header");
+let sticky = document.getElementById("first").offsetHeight;
 
 window.addEventListener("scroll", ()=>{
-    if(getY(artname)+130>getY(document.getElementById('second'))){
+    if(getY(artname)+130>document.getElementById('second').offsetHeight){
         artname.classList.add('cont');
         header.classList.add("ready");
     }
@@ -10,24 +11,54 @@ window.addEventListener("scroll", ()=>{
         artname.classList.remove('cont');
         header.classList.remove("ready");
     }
+
+    if(window.pageYOffset > sticky){
+        header.classList.add("sticky");
+    }
+    else{
+        header.classList.remove("sticky");
+    }
+
+    if(window.pageYOffset>10){
+        for(let i of document.getElementsByClassName("first-wh")){
+            i.classList.add("first-hide")
+        }
+    }
+    else{
+        for(let i of document.getElementsByClassName("first-wh")){
+            i.classList.remove("first-hide");
+        }
+    }
 })
 
 function getY(elem){
-    let box = elem.getBoundingClientRect()
-    return box.top + pageYOffset
+    return elem.getBoundingClientRect().top + window.pageYOffset;
 }
 
-window.onscroll = ()=>{
-    myfunction();
-}
+let releases = document.getElementsByClassName("release-hover");
+let bgRelease = document.getElementById('background-release') // Фон 1
+let bgRelease2 = document.getElementById('background-release2') // Фон 2
+let bgselect = false // Порядок фонов
 
-
-var sticky = document.getElementById("first").offsetHeight
-function myfunction(){
-    if(window.pageYOffset > sticky){
-        header.classList.add("sticky")
-    }
-    else{
-        header.classList.remove("sticky")
-    }
+for(let i of releases){
+    i.addEventListener("mouseover", ()=>{
+        if(!bgselect){ // Если фон 1
+            if(bgRelease.style.backgroundImage != "url(\""+ i.querySelector("img").getAttribute("src") +"\")"){ 
+                // Если сейчас фон не такой
+                bgRelease2.style.backgroundImage = "url(\""+ i.querySelector("img").getAttribute("src") +"\")";
+                bgRelease.classList.add('test');
+                bgRelease2.classList.remove('test');
+                bgselect = true;
+            }
+        }
+        else{ // Если фон 2
+            if(bgRelease2.style.backgroundImage != "url(\""+ i.querySelector("img").getAttribute("src") +"\")"){
+                // Если сейчас фон не такой
+                bgRelease.style.backgroundImage = "url(\""+ i.querySelector("img").getAttribute("src") +"\")";
+                bgRelease.classList.remove('test');
+                bgRelease2.classList.add('test');
+                bgselect = false;
+            }
+        }
+    })
 }
